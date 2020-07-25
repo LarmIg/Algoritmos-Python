297f9a5855b9fe99c9ff26c9bfadc0ac6927e11f
Larmlg <64363606+LarmIg@users.noreply.github.com>
2020-07-25 09:09:43 -0300

    Update Exercicio I.py

--- Capitulo 3/Exercicio I.py
+++ Capitulo 3/Exercicio I.py
@@ -1,4 +1,3 @@
-# Elaborar um programa que apresente o valor da conversão em real (R$) de um valor lido em dólar(US$). O programa deve solicitar o valor da cotação do dólar e também a quantidade de dólares disponível com o usuário. 
 # Elaborar um programa que apresente o valor da conversão em dólar (US$) de um valor lido em real(R$). O programa deve solicitar o valor da cotação do dólar e também a quantidade de reais disponível com o usuário. 
 real = float(input('Informe em dollar para converter em real: '))
 dollar = real / 5.31
